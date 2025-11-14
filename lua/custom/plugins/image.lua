d200dbead50a941a3bf5a23aea2364614741c0f2
nawashiro <hooochiba@gmail.com>
2025-11-14 20:33:41 +0900

    タイポ

--- lua/custom/plugins/image.lua
+++ lua/custom/plugins/image.lua
@@ -1,4 +1,4 @@
-{
+return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
@@ -6,8 +6,8 @@
         resolve = function(path, src)
             if require("obsidian.api").path_is_note(path) then
             return require("obsidian.api").resolve_image_path(src)
-            end,
+            end
         end,
     },
-  }
+  },
 }
