b54445c71ae672f7cfbc67c7bcb9f6dee3649fa0
nawashiro <hooochiba@gmail.com>
2025-11-15 00:11:18 +0900

    update

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -1,25 +1,40 @@
 return {
     "hrsh7th/nvim-cmp",
     version = '*',
-    mapping = {
-        ["<Tab>"] = require('cmp').mapping(function(fallback)
-            if require('cmp').visible() then
-                require('cmp').select_next_item()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-                feedkey("<Plug>(vsnip-expand-or-jump)", "")
-            elseif has_words_before() then
-                require('cmp').complete()
-            else
-                fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-            end
-        end, { "i", "s" }),
+    config = function()
+        local cmp = require("cmp")
 
-        ["<S-Tab>"] = require('cmp').mapping(function()
-            if require('cmp').visible() then
-                require('cmp').select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                feedkey("<Plug>(vsnip-jump-prev)", "")
-            end
-        end, { "i", "s" }),
-    },
+        cmp.setup({
+            mapping = {
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif vim.fn["vsnip#available"](1) == 1 then
+                        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                    end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function()
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                        feedkey("<Plug>(vsnip-jump-prev)", "")
+                    end
+                end, { "i", "s" }),
+
+                -- ドキュメントスクロール
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+                -- 補完のトリガ / キャンセル / 確定
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            },
+        }),
+    end,
 }
