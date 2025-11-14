9a2902443356d1fd4f0cce78a264f7c535862a55
nawashiro <hooochiba@gmail.com>
2025-11-14 23:56:21 +0900

    update

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -1,24 +1,23 @@
-local cmp = require('cmp')
 return {
-    "hrsh7th/nvim-cmp",
+    "hrsh7th/nvim-vim.cmp",
     version = '*',
     
     mapping = {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
+        ["<Tab>"] = vim.cmp.mapping(function(fallback)
+            if vim.cmp.visible() then
+                vim.cmp.select_next_item()
             elseif vim.fn["vsnip#available"](1) == 1 then
                 feedkey("<Plug>(vsnip-expand-or-jump)", "")
             elseif has_words_before() then
-                cmp.complete()
+                vim.cmp.complete()
             else
                 fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
             end
         end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.select_prev_item()
+        ["<S-Tab>"] = vim.cmp.mapping(function()
+            if vim.cmp.visible() then
+                vim.cmp.select_prev_item()
             elseif vim.fn["vsnip#jumpable"](-1) == 1 then
                 feedkey("<Plug>(vsnip-jump-prev)", "")
             end
