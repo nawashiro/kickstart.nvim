66537d10102d6d0c73b484b40789d8cda81babee
nawashiro <hooochiba@gmail.com>
2025-11-14 23:53:56 +0900

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,5 +11,5 @@ return {
     require 'kickstart.plugins.gitsigns',
     require 'custom.plugins.obsidian',
     require 'custom.plugins.image',
-    require 'custom.plugins.nvim-cmp',
+    require 'custom.plugins.cmp',
 }
