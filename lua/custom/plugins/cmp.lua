7e5633ad5a15877d443adba748fa04bb77d009d6
nawashiro <hooochiba@gmail.com>
2025-11-22 08:57:28 +0000

    update

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -1,40 +1,52 @@
 return {
-    "hrsh7th/nvim-cmp",
-    version = '*',
-    config = function()
-        local cmp = require("cmp")
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    'hrsh7th/cmp-buffer', -- バッファ内のテキスト用ソース [1]
+    'hrsh7th/cmp-path', -- ファイルシステムパス用ソース [1]
+    {
+      'L3MON4D3/LuaSnip',
+      version = 'v2.*', -- LuaSnipのスニペットエンジン [1]
+      build = 'make install_jsregexp',
+    },
+    'rafamadriz/friendly-snippets', -- スニペットコレクション [1]
+  },
+  config = function()
+    local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+    require('luasnip.loaders.from_vscode').lazy_load() -- VSCode形式のスニペットをロード [1]
 
-        cmp.setup({
-            mapping = {
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif vim.fn["vsnip#available"](1) == 1 then
-                        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-                    elseif has_words_before() then
-                        cmp.complete()
-                    else
-                        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-                    end
-                end, { "i", "s" }),
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert {
+        -- ドキュメントを下にスクロール [1]
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        -- ドキュメントを上にスクロール [1]
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        -- 補完ウィンドウを開く [1]
+        ['<C-Space>'] = cmp.mapping.complete(),
+        -- 補完ウィンドウを閉じる [1]
+        ['<C-e>'] = cmp.mapping.close(),
+        -- 補完を確定する（置換し、選択を有効にする） [1]
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+      },
+      sources = cmp.config.sources {
+        { name = 'nvim_lsp' }, -- LSPクライアントから補完を取得 [1]
+        { name = 'luasnip' }, -- LuaSnipからスニペットを取得 [1]
+        { name = 'buffer' }, -- 現在のバッファから単語を取得 [1]
+        { name = 'path' }, -- ファイルパスから補完を取得 [1]
+      },
+    }
 
-                ["<S-Tab>"] = cmp.mapping(function()
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                        feedkey("<Plug>(vsnip-jump-prev)", "")
-                    end
-                end, { "i", "s" }),
-
-                -- ドキュメントスクロール
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-                -- 補完のトリガ / キャンセル / 確定
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            },
-        })
-    end,
+    -- Neovimの組み込み設定を調整 [1]
+    vim.cmd [[ set completeopt=menuone,noinsert,noselect highlight! default link CmpItemKind CmpItemMenuDefault ]]
+  end,
 }
+
